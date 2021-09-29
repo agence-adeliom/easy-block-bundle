08db8d46027dc412d2cc316660999043687e9ed2
Arnaud RITTI <arnaud.ritti@adeliom.com>
2021-09-29 12:26:10 +0200

    Fix class

--- src/Block/AbstractBlock.php
+++ src/Block/AbstractBlock.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AbstractBlock extends AbstractType
+abstract class AbstractBlock extends AbstractType
 {
     /**
      * @var EntityManagerInterface
