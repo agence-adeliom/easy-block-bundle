e68d6426fe3441ef3f7f6d9dc031c934d15d2a86
Arnaud RITTI <arnaud.ritti@adeliom.com>
2021-10-04 13:32:49 +0200

    Make command

---
+++ src/Resources/skeleton/block/Block.tpl.php
@@ -0,0 +1,29 @@
+<?= "<?php\n" ?>
+
+namespace <?= $namespace; ?>;
+
+use Adeliom\EasyBlockBundle\Block\AbstractBlock;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class <?= $class_name; ?> extends AbstractBlock<?= "\n" ?>
+{
+    public function buildBlock(FormBuilderInterface $builder, array $options): void
+    {
+        // Implement with your fields
+    }
+
+    public function getName(): string
+    {
+        return '<?= $class_name; ?>';
+    }
+
+    public function getIcon(): string
+    {
+        return '';
+    }
+
+    public function getTemplate(): string
+    {
+        return "<?= $template_name ?>";
+    }
+}
